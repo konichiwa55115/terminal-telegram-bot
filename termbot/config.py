584cf5815de2973aae9b189c3b0c4e7ad1a5afd8
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-03-21 04:31:29 +0200

    Update config.py

--- termbot/config.py
+++ termbot/config.py
@@ -1,9 +1,9 @@
 from termbot.sample_config import Config
 
 class Development(Config):
-  APP_ID = 6
-  API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
-  TG_BOT_TOKEN = "123456789:eb98ae0f7-Gm_6d4abfb49dc_9dc3eBaEBa"
+  APP_ID = 17983098
+  API_HASH = "ee28199396e0925f1f44d945ac174f64"
+  TG_BOT_TOKEN = "5714654934:AAFm0UBvzuU1X-Adg7QThWCzpoKBww9SNXE"
   AUTH_USERS = [
-    7351948
+    6234365091
   ]
