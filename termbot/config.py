c1b87e38638cacd08f94bd59df326055248b555a
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-03-21 04:28:59 +0200

    Create config.py

---
+++ termbot/config.py
@@ -0,0 +1,9 @@
+from termbot.sample_config import Config
+
+class Development(Config):
+  APP_ID = 6
+  API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+  TG_BOT_TOKEN = "123456789:eb98ae0f7-Gm_6d4abfb49dc_9dc3eBaEBa"
+  AUTH_USERS = [
+    7351948
+  ]
